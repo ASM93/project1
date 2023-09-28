4d9de418233711df511f0eea6ee4b442c9c5b2b8
Alexandre Smadja <alexandresmadja@MacBook-Pro-de-Alexandre.local>
2023-09-29 00:44:58 +0200

    tree

--- src/index.js
+++ src/index.js
@@ -9,77 +9,117 @@ class Game {
     this.waveDur = 30;
     this.waveNum = 0;
     this.objFreq = 20;
-    this.lifeStart = 3;
+    this.treeFreq = 15;
+    this.lifeStart = 10;
     this.strengthStart = 1;
     this.mobs = [];
+    this.tree = [];
     this.hero = {};
     this.objects = [];
     this.security = 0;
   }
+
+  /** INIT THE GAME
+   */ ////////////////////////
+
   initialize() {
     this.security += 1;
     if (this.security === 1) {
       //Clean the map
       this.mobs = [];
       this.objects = [];
+      this.tree = [];
       this.hero = {};
       this.waveNum = 0;
       const allCard = document.querySelectorAll(".board-card");
       for (let element of allCard) {
         element.remove();
       }
+      board.innerHTML = "";
       const timer = document.getElementById("timing-box");
       timer.innerHTML = "";
 
-      // Set Start Life
-      const life = document.getElementById("life-box");
-      life.innerHTML = "";
-      hero.life = this.lifeStart;
-      for (let i = 0; i < this.lifeStart; i++) {
-        life.innerHTML += "💛";
-      }
-      // Set Start Strength
-      const strength = document.getElementById("strength-box");
-      strength.innerHTML = "";
-      for (let i = 0; i < this.strengthStart; i++) {
-        strength.innerHTML += "🏹";
-      }
-
-      // Security block
-      let intervalID5 = setInterval(() => {
-        clearInterval(intervalID5);
-      }, 500);
-
-      //Generate the board_cards
-      for (let i = 1; i <= this.boardSize[0]; i++) {
-        for (let j = 1; j <= this.boardSize[1]; j++) {
-          this.boards.push({ i, j });
-          let divCard = document.createElement("div");
-          board.appendChild(divCard);
-          divCard.className = "board-card grass";
-          divCard.id = [i, j];
-          let width = 100 / this.boardSize[1];
-          let height = 80 / this.boardSize[0];
-          divCard.style.width = `${width}%`;
-          divCard.style.height = `${height}vh`;
-        }
-      }
-      // Spawn the hero and start the game
+      // Spawn and start the game
+      this.spawnMap();
+      this.spawnMob();
+      this.spawnTree();
+      this.setData();
+      this.checkStatus();
       const startPosition = [21, Math.round(this.boardSize[1] / 2)];
       hero.spawn(startPosition, "up");
       this.hero = hero;
-      game.run();
+      this.run();
 
       // Release security
       let intervalID4 = setInterval(() => {
         this.security = 0;
-        console.log("secu : " + this.security);
         clearInterval(intervalID4);
       }, 3000);
     }
-    console.log("secu : " + this.security);
   }
-  run() {
+
+  /** CHECK THE GAME
+   */ ////////////////////////
+
+  checkStatus() {
+    if (hero.life <= 0) {
+      board.innerHTML = "LOST";
+      board.innerHTML.style = "text-align:center";
+      clearInterval(intervalID3);
+    }
+  }
+
+  /** GENERATE
+   */ ////////////////////////
+
+  setData() {
+    // Set Start Life
+    const life = document.getElementById("life-box");
+    life.innerHTML = "";
+    hero.life = this.lifeStart;
+    for (let i = 0; i < this.lifeStart; i++) {
+      life.innerHTML += "💛";
+    }
+    // Set Start Strength
+    const strength = document.getElementById("strength-box");
+    strength.innerHTML = "";
+    for (let i = 0; i < this.strengthStart; i++) {
+      strength.innerHTML += "🏹";
+    }
+
+    // Set Timer
+    let time = this.waveDur;
+    const timer = document.getElementById("timing-box");
+    let intervalID3 = setInterval(() => {
+      timer.innerHTML = time + "s";
+      time--;
+      if (time == 0) {
+        clearInterval(intervalID3);
+      }
+      if (hero.life === 0) {
+        clearInterval(intervalID3);
+      }
+    }, 1000);
+  }
+
+  spawnMap() {
+    //Generate the board_cards
+    for (let i = 1; i <= this.boardSize[0]; i++) {
+      for (let j = 1; j <= this.boardSize[1]; j++) {
+        this.boards.push({ i, j });
+        let divCard = document.createElement("div");
+        board.appendChild(divCard);
+        divCard.className = "board-card grass";
+        divCard.id = [i, j];
+        let width = 100 / this.boardSize[1];
+        let height = 80 / this.boardSize[0];
+        divCard.style.width = `${width}%`;
+        divCard.style.height = `${height}vh`;
+      }
+    }
+  }
+
+  spawnMob() {
     // First wave
     let intervalID = setInterval(() => {
       this.waveNum += 1;
@@ -88,8 +128,8 @@ class Game {
       let mobNum = this.startMob + (this.waveNum - 1) * this.waveAcc;
       for (let i = mobNum; i > 0; i--) {
         let mob = new Mob();
-        let mobX = Math.round(Math.random() * this.boardSize[0]);
-        let mobY = Math.round(Math.random() * this.boardSize[1]);
+        let mobX = 1 + Math.floor(Math.random() * this.boardSize[0]);
+        let mobY = 1 + Math.floor(Math.random() * this.boardSize[1]);
         mob.spawn(mobX, mobY, this.waveNum);
         this.mobs.push(mob);
         console.log(this.mobs);
@@ -99,32 +139,40 @@ class Game {
       clearInterval(intervalID);
     }, 500);
 
-    // Set Timer
-    let time = this.waveDur;
-    const timer = document.getElementById("timing-box");
-    let intervalID5 = setInterval(() => {
-      timer.innerHTML = time + "s";
-      time--;
-      if (time == 0) {
-        clearInterval(intervalID5);
-      }
-    }, 1000);
-
     // Spawn Mob every wave
     // let intervalID2 = setInterval(() => {
     // this.waveNum += 1;
     // let mobNum = this.startMob + (this.waveNum - 1) * this.waveAcc;
     // for (let i = mobNum; i > 0; i--) {
     //   let mob = new Mob();
-    //   let mobX = Math.round(Math.random() * this.boardSize[0]);
-    //   let mobY = Math.round(Math.random() * this.boardSize[1]);
+    //   let mobX = 1+Math.floor(Math.random() * this.boardSize[0]);
+    //   let mobY = 1+Math.floor(Math.random() * this.boardSize[1]);
     //   mob.spawn(mobX, mobY, this.waveNum);
     // this.mobs.push(mob);
     //   //   }
     // }, this.waveDur * 1000);
+  }
 
-    // Make Mobs move
+  spawnTree() {
+    for (let i = this.treeFreq; i > 0; i--) {
+      let treeX = 1 + Math.floor(Math.random() * this.boardSize[0]);
+      let treeY = 1 + Math.floor(Math.random() * this.boardSize[1]);
+      let tree = [treeX, treeY];
+      this.tree.push(tree);
+      console.log(tree);
+
+      //Initializing 1st treeCard
+      const treeCard = document.getElementById(tree);
+      treeCard.className = "board-card tree";
+    }
+  }
+
+  /** RUN THE GAME
+   */ ////////////////////////
+
+  run() {
     let intervalID3 = setInterval(() => {
+      // Make Mobs move
       let heroPosition = hero.position;
       // Loop for each Mob
       for (let element of this.mobs) {
@@ -257,8 +305,6 @@ class Game {
       });
     }, 1000);
   }
-
-  checkStatus() {}
 }
 
 //////////////////////////////////////////////////
@@ -273,6 +319,7 @@ class Mob {
     this.createDate = 0;
     this.wave = 0;
   }
+
   spawn(x, y, wave) {
     this.position = [x, y];
     this.wave = wave;
@@ -280,9 +327,9 @@ class Mob {
 
     //Initializing 1st mobCard
     const mobCard = document.getElementById([this.position]);
-
     mobCard.className = "board-card mob";
   }
+
   move(x, y) {
     let oldMobCard = document.getElementById([this.position]);
 
@@ -290,7 +337,6 @@ class Mob {
     this.position[1] = this.position[1] + y;
 
     // Modify mobCard
-
     oldMobCard.className = "board-card grass";
     let newMobCard = document.getElementById([this.position]);
     newMobCard.className = "board-card mob";
@@ -298,9 +344,22 @@ class Mob {
 
   attack() {
     hero.life--;
-    // console.log(hero.life);
+
+    // Set New Life
     const life = document.getElementById("life-box");
-    let remainingLife = life.innerHTML.substring(0, life.innerHTML.length);
+    life.innerHTML = "";
+    for (let i = 0; i < hero.life; i++) {
+      life.innerHTML += "💛";
+    }
+
+    // Skin attack
+    let mobCard = document.getElementById(this.position);
+    mobCard.className += "-attack";
+    // Clear Mob skin
+    let intervalID4 = setInterval(() => {
+      mobCard.className = "board-card mob";
+      clearInterval(intervalID4);
+    }, 100);
   }
 }
 
@@ -316,14 +375,13 @@ class Hero {
     this.strength = 1;
     this.attackMode = "auto";
   }
+
   spawn(startPosition) {
     this.position = startPosition;
     const heroCard = document.getElementById([this.position]);
     heroCard.className = "board-card hero hero-up";
   }
 
-  checkLife() {}
-  die() {}
   move(x, y, dir) {
     let oldHeroCard = document.getElementById([this.position]);
     oldHeroCard.className = "board-card grass";
@@ -352,6 +410,7 @@ class Hero {
       newHeroCard.className = `board-card hero hero-${dir}`;
     }
   }
+
   action() {
     //Check direction
     let heroCard = document.getElementById(this.position);
@@ -359,6 +418,7 @@ class Hero {
     heroCard.className += "-attack";
     let x = 0;
     let y = 0;
+
     let dir = heroCard.className.substring(
       heroCard.className.indexOf("hero-") + 5,
       heroCard.className.length
@@ -442,7 +502,7 @@ btnRight.addEventListener("click", () => {
   game.initialize();
 });
 
-// Move Hero
+// Move Hero KeyPad
 window.addEventListener(
   "keydown",
   function (event) {
@@ -473,3 +533,23 @@ window.addEventListener(
   },
   true
 );
+
+// Move Hero Picture
+
+const key2 = document.getElementById("keyPad2");
+key2.addEventListener("click", () => {
+  hero.move(-1, 0, "up");
+});
+const key4 = document.getElementById("keyPad4");
+key4.addEventListener("click", () => {
+  hero.move(0, -1, "left");
+});
+const key6 = document.getElementById("keyPad6");
+key6.addEventListener("click", () => {
+  hero.move(0, 1, "right");
+  console.log("right");
+});
+const key8 = document.getElementById("keyPad8");
+key8.addEventListener("click", () => {
+  hero.move(1, 0, "down");
+});
